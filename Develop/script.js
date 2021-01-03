9adf9d5cc1959b2068a536d266f779e1a28b6550
Guillermo Garcia <ggq0107@hotmail.com>
2021-01-03 18:50:41 -0500

    added prompts and confirms

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,6 @@
   // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Generate Password
-
-//Prompts and confirmations 
-function passwordCriteria() {
-  var passwordLenght = prompt("How many characters in your new password? Between 8 abd 128 characters.");
-}
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -21,4 +14,21 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 generateBtn.addEventListener("click", passwordCriteria);
 
-console.log(passwordLenght)
+//--------- Start -------------------
+
+//-- Password Characters
+
+var lettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numerical = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", "<", ">"];
+
+//-- Promtps and confirms 
+
+function passwordCriteria() {
+  prompt("How many characters do you want your new password to contain?");
+  confirm("Do you want to include special characters?")
+  confirm("Do you want to include upper case letters?")
+  confirm("Do you want to include lower case letters?")
+  confirm("Do you want to include numbers?")
+}
